5e4e8873cbba3cf0bad159d2d4dde54b331fe577
dnldl <dnldlscjf@gmail.com>
2024-04-02 18:40:24 +0900

    인터페이스로 Service 메서드만 선언

---
+++ src/main/java/com/example/test_2/service/FileStorageService.java
@@ -0,0 +1,13 @@
+package com.example.test_2.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+public interface FileStorageService {
+    String storeFile(MultipartFile file);
+    Resource loadFileAsResource(String fileName);
+    List<String> loadAllFiles();
+    public void deleteFile(String fileName);
+}
+
